41a3b5f580bae628ebb824b4fffb680d2b5e6229
Yaqub Mahmoud <yaqub320@gmail.com>
2019-04-29 22:13:02 +0900

    Update index.js

--- Randomized-Slashes-Project/js/index.js
+++ Randomized-Slashes-Project/js/index.js
@@ -2,6 +2,7 @@ function generate(){
   
   var length = 29
   var height = 100
+  
   for (var line=1; line < height; line++) {
     for(var i=1; i<length; i++) {
       var output = (Math.floor((Math.random() * 2) % 2) ) ? "╱" : "╲";

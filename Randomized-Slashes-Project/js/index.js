2e2a901c95f889abe739c31b87c3572ff1163484
Yaqub Mahmoud <yaqub320@gmail.com>
2019-04-29 22:12:48 +0900

    Update index.js

--- Randomized-Slashes-Project/js/index.js
+++ Randomized-Slashes-Project/js/index.js
@@ -4,11 +4,11 @@ function generate(){
   var height = 100
   for (var line=1; line < height; line++) {
     for(var i=1; i<length; i++) {
-      var output = (Math.floor((Math.random()*2)%2)) ? "╱" : "╲";
+      var output = (Math.floor((Math.random() * 2) % 2) ) ? "╱" : "╲";
       document.write(output);
     }
 
-    
+
   document.writeln("<br>");
  }
 }
